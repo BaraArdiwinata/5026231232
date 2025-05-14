aa7df520a1b4a072f23b0f99389be00a00a47b33
BaraArdiwinata <baraardi53@gmail.com>
2025-05-14 12:21:07 +0700

    Pertemuan 11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+    <h3>www.malasngoding.com</h3>
+    <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+    <p>Ini adalah view blog. ada di route blog.</p>
+    <?php
+    $nama = 'Malas Ngoding';
+    echo 'Hello Apa Kabar?' . $nama;
+    ?>
+    <br>
+    {{ 'Hello Apa Kabar?' . $nama }}
+    <br>
+    <?php
+    date_default_timezone_set('Asia/Jakarta');
+    echo date('d-m-Y H:i:s'); // Hasil: 20-01-2017 05:32:15
+    ?>
+
+</body>
+</html>
