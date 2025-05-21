b1e7bdce4002446230c171a0c3d228eda8131e6f
BaraArdiwinata <baraardi53@gmail.com>
2025-05-21 12:35:25 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 // import java.io;
 
 
@@ -39,3 +41,15 @@ Route::get('Latihan', [Link::class, 'Latihan']);
 Route::get('frontend', [Link::class, 'frontend']);
 
 Route::get('ets', [Link::class, 'ets']);
+
+Route::get('dosen', [Link::class, 'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// Route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
