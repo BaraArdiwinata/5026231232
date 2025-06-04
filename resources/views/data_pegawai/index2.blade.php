bd9c5a8e6bc3d217fe0221c9cedaafa695c38e68
BaraArdiwinata <baraardi53@gmail.com>
2025-06-04 12:34:09 +0700

    Pertemuan 14

--- resources/views/data_pegawai/index2.blade.php
+++ resources/views/data_pegawai/index2.blade.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('template')
 
-	<h2>www.malasngoding.com</h2>
+@section('content')
 	<h3>Data Pegawai</h3>
+	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+	<p>Cari Data Pegawai :</p>
+	<form action="/pegawai/cari" method="GET">
+		<input type="text" class="form-control" name="cari" placeholder="Cari Pegawai .."  value="$cari">
+		<input type="submit" class="btn btn-info" value="CARI">
+	</form>
 
-	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
-
-	<br/>
-	<br/>
-
-	<table border="1">
+    <br/>
+	<table class="table table-striped">
 		<tr>
 			<th>Nama</th>
 			<th>Jabatan</th>
@@ -22,78 +19,17 @@
 			<th>Opsi</th>
 		</tr>
 		@foreach($pegawai as $p)
-		<tr>
-			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-			<td>
-				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
-				|
-				<a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
-			</td>
-		</tr>
+        <tr>
+            <td>{{ $p->pegawai_nama }}</td>
+            <td>{{ $p->pegawai_jabatan }}</td>
+            <td>{{ $p->pegawai_umur }}</td>
+            <td>{{ $p->pegawai_alamat }}</td>
+            <td>
+                <a href="/pegawai/edit/{{$p->pegawai_id}}" class="btn btn-success">Edit</a>
+                <a href="/pegawai/hapus/{{$p->pegawai_id}}" class="btn btn-danger">Hapus</a>
+            </td>
+        </tr>
 		@endforeach
 	</table>
-
-
-</body>
-</html>
-belajar_laravel/resources/views/tambah.blade.php
-
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
-
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
-
-	<a href="/pegawai"> Kembali</a>
-
-	<br/>
-	<br/>
-
-	<form action="/pegawai/store" method="post">
-		{{ csrf_field() }}
-		Nama <input type="text" name="nama"> <br/>
-		Jabatan <input type="text" name="jabatan"> <br/>
-		Umur <input type="number" name="umur"> <br/>
-		Alamat <textarea name="alamat"></textarea> <br/>
-		<input type="submit" value="Simpan Data">
-	</form>
-
-
-
-</body>
-</html>
+    {{ $pegawai->links() }}
+@endsection
